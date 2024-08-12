681e3a4f49b05897b94a6700d11d1b974b0092af
happysubin <asb0711@gmail.com>
2024-08-12 22:41:18 +0900

    first commit

---
+++ src/test/kotlin/com/my/template/TemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.my.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
